934019f8b3a24ec7cc5f644d89746544016b0cff
RimohtL <114160831+RimohtL@users.noreply.github.com>
2023-11-23 10:33:59 +0100

    Updated formulas
    There might be an error somewhere because the default proba is too close from zero.

--- hurst_v2.py
+++ hurst_v2.py
@@ -7,7 +7,7 @@ from scipy.optimize import curve_fit
 
 """ Data Importation """
 
-path = "C:/Users/Amaury/Documents/CentraleSupelec/3A/Projet/"
+path = "/Users/user/Documents/Cours/CentraleSupelec/Projet/Docs/"
 file = path + "Data issuers.xlsx"
 market_cap = pd.read_excel(file, sheet_name="Mod Market Cap")
 market_cap = market_cap.set_index("Dates").loc['2019-10-28':'2020-10-13']
@@ -21,7 +21,11 @@ def d1(x, sigma_A, t, T, H, rf, company_debt):
 
 def d2(x, sigma_A, t, T, H,rf,company_debt):
     return ((np.log(x / company_debt)) + rf*(T-t) - 0.5 * sigma_A ** 2*(T**(2*H)-t**(2*H) )/ (
-    sigma_A*np.sqrt(T**(2*H)-t**(2*H))  ))
+    sigma_A*np.sqrt(T**(2*H)-t**(2*H)) ))
+
+def d2_hurst(x, sigma_A, t, T, H,rf,company_debt):
+    return ((np.log(x / company_debt)) + rf*(T-t) - 0.5 * sigma_A ** 2*(T**(2*H)-t**(2*H) )/ (
+    sigma_A*np.sqrt((T-t)**(2*H)) ))
 
 # inverse the black scholes formula
 def merton_formula(x, rf, t ,T, H,company_debt,equity_value,sigma_A):
@@ -36,85 +40,89 @@ def update_values(Var,frequency,sigma_A):
     return(sigma_A,sigma_A_former,H)
 
 def var_relation(f,sigma,H):
-    return(np.exp(2* np.log(sigma/(np.exp(H*np.log(f))) )) )
+    return (np.exp(2* np.log(sigma/(np.exp(H*np.log(f))))))
 
-def update_values2(Var,frequency,sigma_A,H):
+def update_values2(Var, frequency, sigma_A, H):
     float_frequency=[float(i) for i in frequency]
     sigma_A_former=sigma_A
-    popt, pcov = curve_fit(var_relation,float_frequency,Var,p0=np.array([sigma_A,H]))
+    popt, pcov = curve_fit(var_relation, float_frequency, Var, p0=np.array([sigma_A,H]))
     sigma_A,H= popt[0], popt[1]
 
-    ydata=np.array(Var)
-    y=np.array([var_relation(f,sigma_A,H) for f in frequency])
-    residuals = np.array(ydata- y)
+    ydata = np.array(Var)
+    y = np.array([var_relation(f, sigma_A, H) for f in frequency])
+    residuals = np.array(ydata - y)
     ss_res = np.sum(residuals**2)
     ss_tot = np.sum((ydata-np.mean(ydata))**2)
     r_squared = 1 - (ss_res / ss_tot)
-    print('R_squared=',r_squared)
-    return(sigma_A,sigma_A_former,H)
+    print('R_squared=', r_squared)
+    return(sigma_A, sigma_A_former, H)
 
 
-def BSM_H(ticker, market_cap, debt, T=1, delta=[1,2,3,4,5,6,7,8,9,10], rf=0, epsilon=10e-3,H0=0.5):
+def BSM_H(ticker, market_cap, debt, T=1, delta=[1, 2, 3, 4, 5, 6, 7, 8, 9, 10], rf=0, epsilon=10e-3, H0=0.5):
 
-    frequency=[]
+    frequency = []
     for d in delta:
         frequency.append(252//d)
     company_debt = debt[[ticker]].iloc[0, 0]
     company_market_cap = market_cap[[ticker]].iloc[:, 0]
-    equity_value = 0
     sigma_A_former = 0
-    H=H0
-    asset_values = []
+    H = H0
     sigma_E = np.std(np.diff(np.log(company_market_cap), n=1)) * np.sqrt(frequency[0])
     sigma_A = sigma_E
-    
-    n_iter=1
+
+    n_iter = 1
     while np.abs(sigma_A - sigma_A_former) > epsilon:
-        print("Iteration ",n_iter)
+        print("Iteration ", n_iter)
 
         asset_values = {}
         for f in frequency:
-            fasset_values=[]
-            n=company_market_cap.shape[0]
-            days=[]
+            fasset_values = []
+            n = company_market_cap.shape[0]
+            days = []
             for i in range(n):
-                if i%(n//f)==0:
+                if i % (n//f) == 0:
                     days.append(i)
             for day in days:
-                t= day/ n
-                #print(t)
+                t = day / n
                 equity_value = company_market_cap[day]
                 # find zero of Merton function, ie asset_value at the current_time
-                fasset_values.append(optimize.newton(merton_formula, company_debt, args=(rf,t,1+T,H,company_debt,equity_value,sigma_A),maxiter=100))
-            asset_values[f]=fasset_values
- 
+                fasset_values.append(optimize.newton(merton_formula, company_debt, args=(rf, t, 1+T, H, company_debt, equity_value, sigma_A), maxiter=100))
+            asset_values[f] = fasset_values
+
 
         # update values
-        Var=[]
+        Var = []
         for f in frequency:
-            Var.append( np.var(np.diff(np.log(asset_values[f]), n=1)) )
+            Var.append(np.var(np.diff(np.log(asset_values[f]), n=1)))
 
-        sigma_A,sigma_A_former,H=update_values2(delta,asset_values,sigma_A,H)
-        print("sigma=",sigma_A,"  H=",H)
-        n_iter+=1
-        #print(asset_values)
-        
+        sigma_A, sigma_A_former, H = update_values2(delta, asset_values, sigma_A, H)
+        print(f"sigma= {sigma_A} %, H={H}")
+        n_iter += 1
 
     # compute distance to default and default probability
-    distance_to_default = d2(asset_values[frequency[0]][-1], sigma_A, 0, T,H,rf,company_debt)
+    distance_to_default = - d2_hurst(company_market_cap.iloc[-1], sigma_A, 1, 1+T, H, rf, company_debt)
     default_probability = (1 - norm.cdf(distance_to_default)) * 100
     return distance_to_default, default_probability
-    
-    
-
-""" Test 
 
 
+# REMINDER : T = Debt Maturity => so if we want T = 1 then T = Trading period + 1
+# Trading period = 252 days here
+
+"""
+cols = []
 for ticker in market_cap.columns:
     if ticker in debt.columns:
-        distance_to_default, default_probability = BSM_H(ticker, market_cap, debt)
-        print(f"{ticker} \nDistance to default {round(distance_to_default, 3)}, Default Probability {round(default_probability, 3)} \n")
+        cols.append(ticker)
+        
+results = pd.DataFrame(index=["Sigma", "Distance to default", "Default Probability"], columns=cols)
+
+for ticker in cols:
+    results[ticker] = BSM(ticker, market_cap, debt)
+
+print(results["CRH LN Equity"])
+print(results)
 """
-ticker=market_cap.columns[0]
+
+ticker = market_cap.columns[1]
 distance_to_default, default_probability = BSM_H(ticker, market_cap, debt)
 print(f"{ticker} \nDistance to default {round(distance_to_default, 3)}, Default Probability {round(default_probability, 3)} \n")
